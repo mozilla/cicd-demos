67bb3d6c1532cd4f0b4d3d591dc31e98ac7b7e1a
Dustin Lactin <dlactin@mozilla.com>
2024-11-20 16:31:13 -0700

    feat(go-demo): initial commit of go-demo app, with github action to build and push image to GAR

---
+++ go-demo/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/__version__", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		// Read version.json file
+		versionFile, err := os.Open("/app/version.json")
+		if err != nil {
+			http.Error(w, "Failed to read version.json", http.StatusInternalServerError)
+			log.Println("Error reading version.json:", err)
+			return
+		}
+		defer versionFile.Close()
+
+		// Read the content of the file
+		data, err := io.ReadAll(versionFile)
+		if err != nil {
+			http.Error(w, "Failed to read version.json content", http.StatusInternalServerError)
+			log.Println("Error reading version.json content:", err)
+			return
+		}
+
+		// Write the content to the response
+		w.Write(data)
+	})
+
+	http.HandleFunc("/__lbheartbeat__", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	http.HandleFunc("/__heartbeat__", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the cicd demo HTTP app! Visit /__version__ for version information."))
+	})
+
+	port := "8080"
+	println("Server running on port:", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Println("Error starting server:", err)
+	}
+}
